4e4232c718fc7b0fad14e5294f6e0c5104ff3c25
Paresh Agarwal <agarwal.paresh53@gmail.com>
2017-04-22 02:21:27 -0500

    adding the first controller

--- hellorestservice/src/main/java/com/codebleeder/learning/HellorestserviceApplication.java
+++ hellorestservice/src/main/java/com/codebleeder/learning/HellorestserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HellorestserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellorestserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HellorestserviceApplication.class, args);
+    }
 }
