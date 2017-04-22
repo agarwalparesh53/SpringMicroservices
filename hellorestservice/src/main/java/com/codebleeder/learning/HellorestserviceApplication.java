e38706bfb7fba342a470960c6f78d0d5f2062912
Paresh Agarwal <agarwal.paresh53@gmail.com>
2017-04-22 01:08:41 -0500

    adding the rest service helloworld project

---
+++ hellorestservice/src/main/java/com/codebleeder/learning/HellorestserviceApplication.java
@@ -0,0 +1,12 @@
+package com.codebleeder.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellorestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellorestserviceApplication.class, args);
+	}
+}
---
+++ hellorestservice/src/test/java/com/codebleeder/learning/HellorestserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codebleeder.learning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellorestserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
