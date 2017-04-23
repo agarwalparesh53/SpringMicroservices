7b2f7115f15a20b983aa24129f40ad9e8379b7e4
Paresh Agarwal <agarwal.paresh53@gmail.com>
2017-04-23 17:05:29 -0500

    adding the basic security

--- hellohatheos/src/main/java/com/codebleeders/learning/HellohatheosApplication.java
+++ hellohatheos/src/main/java/com/codebleeders/learning/HellohatheosApplication.java
@@ -2,8 +2,10 @@ package com.codebleeders.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity
 public class HellohatheosApplication {
 
 	public static void main(String[] args) {
